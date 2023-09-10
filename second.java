94d6bfc8f11b83c97056eb7982f1f70907ca6a45
rnayakgit <ranjannayak221@gmail.com>
2023-09-10 17:44:10 +0530

    updating local file-1

--- second.java
+++ second.java
@@ -1,6 +1,6 @@
 public class second{
     public static void main(String[] args){
         System.out.println("wecome to github");
-        System.out.println("welcome to github");
+        System.out.println(" FFF to github");
     }
 }
